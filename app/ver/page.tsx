ab809c718c7cd87efef49de910ce00afd4c0ab33
franthony00 <123212565+franthony00@users.noreply.github.com>
2025-12-21 19:45:04 -0400

    Update page.tsx

--- app/ver/page.tsx
+++ app/ver/page.tsx
@@ -4,10 +4,11 @@ import "./dgii.css";
 import { Suspense } from "react";
 import ViewerContent from "@/components/viewer-content";
 
-
 function LoadingFallback() {
   return (
     <div className="dgii-container">
+      
+      {/* HEADER DGII */}
       <div className="dgii-header-green">
         <img
           src="/placa-provisional-header.png"
@@ -16,14 +17,17 @@ function LoadingFallback() {
         />
       </div>
 
+      {/* TÍTULOS */}
       <div className="dgii-title">
         <span className="title-orange">Sistema Datamatrix</span>
         <span className="title-green">Validación de Documentos</span>
       </div>
 
+      {/* CONTENIDO */}
       <div className="dgii-content">
         <p>Cargando información...</p>
       </div>
+
     </div>
   );
 }
