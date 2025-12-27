f2fa1ea6963b801e090058b55e4abb8903d4b21f
franthony00 <123212565+franthony00@users.noreply.github.com>
2025-12-26 23:53:46 -0400

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -4,7 +4,7 @@ if (!process.env.POSTGRES_URL) {
   throw new Error("❌ Missing POSTGRES_URL in environment variables");
 }
 
-export const pool = new Pool({
+export const db = new Pool({
   connectionString: process.env.POSTGRES_URL,
   ssl: {
     rejectUnauthorized: false,
