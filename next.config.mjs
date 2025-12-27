0a2b7bd9c1fa21a558303c153b4496e0df36605f
franthony00 <123212565+franthony00@users.noreply.github.com>
2025-12-26 20:51:40 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // <--- AGREGA ESTA LÍNEA ESENCIAL
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
-  // Si tu repo se llama dgii.gov.do, descomenta la siguiente línea:
-  // basePath: '/dgii.gov.do', 
+  experimental: {
+    serverActions: true,
+  },
 };
 
 export default nextConfig;
