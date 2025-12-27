280d0b26d7d63ee89e56eec2f16ed8200fedf771
franthony00 <123212565+franthony00@users.noreply.github.com>
2025-12-26 21:41:31 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  
   typescript: {
     ignoreBuildErrors: true,
   },
-  
   images: {
     unoptimized: true,
   },
